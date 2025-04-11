bf5aba3b61384bf68b3f8f25252585159637b795
idonun <mc.natsu831@gmail.com>
2025-04-12 03:30:14 +0900

    Set export keyword

--- lib/pazzy.dart
+++ lib/pazzy.dart
@@ -1,5 +1,2 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/pazzy_numberless_widget.dart';
+export 'src/pazzy_widget.dart';
